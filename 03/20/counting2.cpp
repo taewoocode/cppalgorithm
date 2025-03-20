5287b6b198ec95298c01ce62e3b55fa9705d7122
taewoocode <taewoocode@gmail.com>
2025-03-21 03:02:08 +0900

    test

---
+++ 03/20/counting2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> solution(vector<int> lst){
+    sort(lst.begin(), lst.end());
+    for(auto i : v){
+        cout << i << ' ';
+    }
+
+}
+
+int main()[
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    solution()
+]
