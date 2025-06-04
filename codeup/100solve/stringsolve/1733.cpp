6a6254b132788f311696daa4cd03d98590fbfa3b
taewoocode <qkrxodn6035@gmail.com>
2025-06-05 02:02:49 +0900

    test

---
+++ codeup/100solve/stringsolve/1733.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    string s;
+    getline(cin, s);
+
+    if(s == "IOI") cout << "IOI is the International Olympiad in Informatics." << " ";
+    else cout << "I don't care.";
+    
+    return 0;
+}
